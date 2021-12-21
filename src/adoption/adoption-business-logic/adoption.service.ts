7891fe73fd721d5f9f4ea16414f8437875c82e99
Maciej Sikorski <maciejsikorski00@gmail.com>
2021-12-21 09:29:07 +0100

    Add PetType business logic

--- src/adoption/adoption-business-logic/adoption.service.ts
+++ src/adoption/adoption-business-logic/adoption.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { Adoption } from './entities/adoption.entity';
+import { Pet, PetType } from './entities/pet.entity';
 import { AdoptionRepository } from './ports/adoption.repository';
 import { CanClientAdoptPet } from './ports/can-client-adopt-pet';
 import { PetRepository } from './ports/pet.repository';
@@ -26,10 +27,8 @@ export class AdoptionService {
       throw new Error(`This customer can't adopt`);
     }
 
-    if (pet.age < 1) {
-      this.reserve(petId, clientId);
-      throw new Error(`This pet is too young. Reserved`);
-    }
+    this.assertAge(pet, clientId);
+
     await this.adoptionRepository.insert(
       new Adoption(petId, clientId, new Date()),
     );
@@ -37,5 +36,17 @@ export class AdoptionService {
 
   private reserve(petId: string, clientId: string) {
     throw new Error('Method not implemented.');
+  }
+
+  private assertAge(pet: Pet, clientId: string): void {
+    const petId = pet.id;
+    if (pet.type === PetType.Cat && pet.age < 1) {
+      this.reserve(petId, clientId);
+      throw new Error(`This pet is too young. Reserved`);
+    }
+    if (pet.type === PetType.Dog && pet.age < 0.5) {
+      this.reserve(petId, clientId);
+      throw new Error(`This pet is too young. Reserved`);
+    }
   }
 }
--- src/adoption/adoption-business-logic/entities/pet.entity.ts
+++ src/adoption/adoption-business-logic/entities/pet.entity.ts
@@ -1,3 +1,9 @@
+export enum PetType {
+  Dog,
+  Cat,
+}
 export class Pet {
+  type: PetType;
+  id: string;
   constructor(public age: number) {}
 }
